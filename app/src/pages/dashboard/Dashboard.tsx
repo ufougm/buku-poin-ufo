cb81ba25566661aa5a00904c6335d054ed379bd3
ufougm <ukm.foto@ugm.ac.id>
2026-09-07 20:38:30 +0700

    Fix hp

--- app/src/pages/dashboard/Dashboard.tsx
+++ app/src/pages/dashboard/Dashboard.tsx
@@ -86,6 +86,7 @@ export default function MemberDashboard() {
   const [uploadError, setUploadError] = useState("");
 
 console.log("BONGKAR DATA:", JSON.stringify(pemandus, null, 2));
+console.log("Cek HP Pemandu 1:", pemandu1?.phone);
 
   // Image compression utility
   const compressImage = (file: File, maxWidth = 800, quality = 0.6): Promise<string> => {
